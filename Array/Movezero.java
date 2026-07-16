325504ef9303bad8e65cabce466513798cff75a1
Atharva Vaddepalli <v.atharvan@gmail.com>
2026-07-16 19:33:47 +0530

    Fibo no

--- Array/Movezero.java
+++ Array/Movezero.java
@@ -1,2 +1,16 @@
 import java.util.*;
-pub
+public class Movezero {
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size= sc.nextInt();
+        int number[]= new int[size];
+
+        //To take input
+        for (int i =0; i<size; i++){
+            number[]=sc.nextInt();
+        }
+        
+    }
+}
+}
